fa05b39ea1e619568dd52fa9aa674af58d7e18d3
Julia2111 <152428541+Julia2111@users.noreply.github.com>
2024-06-12 20:01:43 +0300

    Delete src directory

--- src/assets/theme.ts
+++
@@ -1,7 +0,0 @@
-import {ReactComponent as dark} from './dark.svg'
-import {ReactComponent as light} from "./light.svg";
-
-export const themeIcons = {
-    dark,
-    light
-}
--- src/pages/SignUp/SignUp.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./SignUp.module.scss";
-import { ReactComponent as Logo } from "../../assets/pixema.svg";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { useAuth } from "../../hook/useAuth";
-import { Action, ThunkDispatch } from "@reduxjs/toolkit";
-import AuthProvider from "../../HOC/AuthProvider";
